aaa73f539ca69d5f2a3d7f1bdf7f2a7f6e77a2c6
lowskii <lowskillboy03@gmail.com>
2024-04-24 23:54:37 +0300

    правки по стилям

--- src/components/CartListItem.vue
+++ src/components/CartListItem.vue
@@ -3,7 +3,7 @@ import CartItem from './CartItem.vue'
 </script>
 
 <template>
-  <div class="flex flex-col gap-4">
+  <div class="flex flex-col flex-1 gap-4">
     <CartItem  img="/sneakers/sneakers-1.jpg" title="Мужские кроссовки Nike Blazer Mid Suede"/>
     <CartItem  img="/sneakers/sneakers-3.jpg" title="Мужские кроссовки"/>
   </div>
