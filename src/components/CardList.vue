69f844400ff56e5bdc80001c0bf7ac487bf31768
lowskii <lowskillboy03@gmail.com>
2024-04-24 23:54:07 +0300

    Добавлены пропсы

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -8,7 +8,8 @@ import Card from './Card.vue'
       img="/sneakers/sneakers-1.jpg"
       title="Мужские кроссовки Nike Blazer Mid Suede"
       :price="12999"
-      :is-added="true"
+      :is-added="false"
+      :is-favorite="true"
       :on-click-add="() => alert(111)"
     />
   </div>
